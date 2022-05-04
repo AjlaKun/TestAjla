0ea5dd1938828a85bb23b3cd32cf9b7379e42cd9
AjlaK <kundicajla@gmail.com>
2022-05-04 13:38:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestAjla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello da  Ajla!");
+        }
+    }
+}

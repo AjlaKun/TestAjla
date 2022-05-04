420046fc76dfba0a86c969aa6f708dedb46680f2
AjlaK <kundicajla@gmail.com>
2022-05-04 18:48:47 +0200

    merge ramo mateo

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ namespace TestAjla
         static void Main(string[] args)
         {
             Console.WriteLine("Hello da  Ajla!");
-            Console.WriteLine("Hello da Gianluca step1!");
-            Console.WriteLine("Hello da Gianluca step2!");
+            Console.WriteLine("Hello da  Matteo");
         }
     }
 }
